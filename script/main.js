b4b9295377e772c2b9b5c991713bc3bd6b6c0638
Roman Bienov <rodergstep@gmail.com>
2016-10-09 21:52:32 +0300

    first download

---
+++ script/main.js
@@ -0,0 +1,88 @@
+$(function() {
+
+	$.scrollify({
+		section:".panel",
+		after:function(i) {
+
+/*
+			if(i===2) {
+				$.scrollify.setOptions({
+					easing:"easeOutExpo"
+				});
+			}*/
+
+		}
+	});
+
+	$(".scroll,.scroll-btn").click(function(e) {
+		e.preventDefault();
+
+		$.scrollify.next();
+	});
+});
+
+
+$(document).ready(function(){
+
+
+/*calling of lightbox plugin */$('a[data-rel^=lightcase]').lightcase();
+
+// Begin of icon-menu transfomation
+    $('#nav-icon1').click(function(){
+      $(this).toggleClass('open');
+    });
+// End of icon-menu transfomation
+
+// Begin of dropdown menu
+    var touch = $('#touch-menu');
+      var menu = $('.nav');
+   
+      $(touch).on('click', function(e) {
+          e.preventDefault();
+          menu.slideToggle();
+      });
+      $(window).resize(function(){
+          var wid = $(window).width();
+          if(wid > 760 && menu.is(':hidden')) {
+              menu.removeAttr('style');
+          }
+      });
+// End of dropdown menu
+    });
+
+/*filtering of tiles*/ 
+(function($) {
+
+  'use strict';
+
+  var $filters = $('.filter [data-filter]'),
+    $boxes = $('.boxes [data-category]');
+
+  $filters.on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    
+    $filters.removeClass('active');
+    $this.addClass('active');
+
+    var $filterColor = $this.attr('data-filter');
+
+    if ($filterColor == 'all') {
+      $boxes.removeClass('is-animated')
+        .fadeOut().finish().promise().done(function() {
+          $boxes.each(function(i) {
+            $(this).addClass('is-animated').delay((i++) * 200).fadeIn();
+          });
+        });
+    } else {
+      $boxes.removeClass('is-animated')
+        .fadeOut().finish().promise().done(function() {
+          $boxes.filter('[data-category = "' + $filterColor + '"]').each(function(i) {
+            $(this).addClass('is-animated').delay((i++) * 200).fadeIn();
+          });
+        });
+    }
+  });
+
+})(jQuery);
+/*END of filtering of tiles*/
